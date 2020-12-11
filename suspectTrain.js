7dcc9551836d7ae5cd7e81ed3e2bde7238cb6da3
Hamide Bay <hamide.bay@baycom.ch>
2020-12-11 23:10:47 +0100

    code formatted

--- suspectTrain.js
+++ suspectTrain.js
@@ -1,31 +1,17 @@
 let tour = 0;
 const wagon = Math.floor(Math.random() * 10) + 1;
 
-for(tour = 1; tour<5; tour++) {
-    let tahmin = prompt("tahminini gir");
+for (tour = 1; tour < 5; tour++) {
+  let tahmin = prompt("tahminini gir");
 
-    if(tahmin == wagon){
-        alert("Bravooo kazandin");
-    }
-    else if(tahmin < wagon){
-          
-        alert("Hirsiz arka vagonlarda");
-    }
-    else{if(tahmin > wagon)
-          
-        alert("Hirsiz ön vagonlarda");
-    }
-    if(tour == 4 && tahmin != wagon){alert("üzgünüm hirsizi bulamadiniz");}
+  if (tahmin == wagon) {
+    alert("Bravooo kazandin");
+  } else if (tahmin < wagon) {
+    alert("Hirsiz arka vagonlarda");
+  } else {
+    if (tahmin > wagon) alert("Hirsiz ön vagonlarda");
+  }
+  if (tour == 4 && tahmin != wagon) {
+    alert("üzgünüm hirsizi bulamadiniz");
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
